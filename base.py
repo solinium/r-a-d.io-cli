3a0d27a6051e5ecb1a433493dfb7bc8d238e3b47
Alexander <asg@airmail.cc>
2017-06-20 16:11:10 -0400

    last

--- base.py
+++ base.py
@@ -4,6 +4,7 @@ import sys
 import json
 import requests
 import datetime
+import pprint
 import webbrowser
 import subprocess
 
@@ -11,7 +12,6 @@ def importAPI():
 	global api
 	apiraw = requests.get(url='https://r-a-d.io/api', headers={'User-agent': 'Mozilla/5.0'})
 	api = apiraw.json()
-
 
 def extractJson():
 	global djName
@@ -47,10 +47,63 @@ def extractJson():
 	global currentTime
 	currentTime = api['main']['current']
 
+def extractQueue():
+
+
+	#test arrays
+
+	pprint.pprint(api)
+
+	#global queueTitle1
+	#queueTitle1 = api['main']['queue']['0']['meta']
+
+	#global queueTime1
+	#queueTime1 = api['main']['queue']['0']['timestamp']
+
+	#global queueTitle2
+	#queueTitle2 = api['main']['queue']['1']['meta']
+
+	#global queueTime2
+	#queueTime2= api['main']['queue']['1']['timestamp']
+	
+	#global queueTitle3
+	#queueTitle3 = api['main']['queue']['2']['meta']
+
+	#global queueTime3
+	#queueTime1 = api['main']['queue']['2']['timestamp']
+
+	#global queueTitle4
+	#queueTitle4 = api['main']['queue']['3']['meta']
+
+	#global queueTime4
+	#queueTime1 = api['main']['queue']['3']['timestamp']
+
+	#global queueTitle5
+	#queueTitle4 = api['main']['queue']['4']['meta']
+
+	#global queueTime5
+	#queueTime1 = api['main']['queue']['4']['timestamp']
+
+def calculateQueueTime(x, y):
+	if y == (1):
+		global queueStartSecs1
+		queueStartSecs1 = (x - currentTime)
+	elif y == (2):
+		global queueStartSecs2
+		queueStartSecs2 = (x - currentTime)
+	elif y == (3):
+		global queueStartSecs3
+		queueStartSecs3 = (x - currentTime)
+	elif y == (4):
+		global queueStartSecs4
+		queueStartSecs = (x - currentTime)
+	elif y == (5):
+		global queueStartSecs5
+		queueStartSecs5= (x - currentTime)
 
 def functionJson():
 	global isAfkStreamStr
-	if isAfkStream == True:
+	if isAfkStream == (True):
 		isAfkStreamStr = ("AFK Stream")
 	else:
 		isAfkStreamStr = ("")
@@ -62,7 +115,12 @@ def functionJson():
 		else:
 			isOldThread = (False)
 
-	
+	calculateQueueTime(queueTime1, 1)
+	calculateQueueTime(queueTime2, 2)
+	calculateQueueTime(queueTime3, 3)
+	calculateQueueTime(queueTime4, 4)
+	calculateQueueTime(queueTime5, 5)
+
 def songTimeLength():
 	global songLengthSeconds
 	songLengthSeconds = (endTime - startTime)
@@ -72,11 +130,10 @@ def songTimeLength():
 
 	tempSongLength = readableSongLength[2:3]
 	global totalSongTime
-	if tempSongLength == 0:
+	if tempSongLength == (0):
 		totalSongTime = readableSongLength[2:7]
 	else:
 		totalSongTime = readableSongLength[3:7]
-
 
 def songTimeLeft():
 	global currentSongcTimeLeft
@@ -87,7 +144,7 @@ def songTimeLeft():
 
 	tempCurrentSongcTimeLeft = readableCurrentSongcTimeLeft[2:3]
 
-	if tempCurrentSongcTimeLeft == 0:
+	if tempCurrentSongcTimeLeft == (0):
 		formattedCurrentSongcTimeLeft = readableCurrentSongcTimeLeft[2:7]
 	else:
 		formattedCurrentSongcTimeLeft = readableCurrentSongcTimeLeft[3:7]
@@ -98,7 +155,6 @@ def songTimeLeft():
 	global currentSongTimeLeft
 	currentSongTimeLeft = formattedCurrentSongcTimeLeft
 
-
 def songTimeCurrent():
 	global currentSongcTime
 	currentSongcTime = (songLengthSeconds - currentSongcTimeLeft)
@@ -108,7 +164,7 @@ def songTimeCurrent():
 
 	tempCurrentSongcTime = readableCurrentSongcTime[2:3]
 
-	if tempCurrentSongcTime == 0:
+	if tempCurrentSongcTime == (0):
 		formattedCurrentSongcTime = readableCurrentSongcTime[2:7]
 	else:
 		formattedCurrentSongcTime = readableCurrentSongcTime[3:7]
@@ -122,12 +178,12 @@ def songTimeCurrent():
 	# totalSongTime is song length
 	# currentSongTime is how far in the song
 	# currentSongTimeLeft is how much time is left
-
+	print(queueStartSecs1)
 
 def openThread():
 	global thread
 	if isOldThread == (False):
-		if sys.platform == 'darwin':	#osx
+		if sys.platform == ('darwin'):	#osx
 			subprocess.Popen(['open', threadUrl])
 		else:
 			webbrowser.get('firefox').open(threadUrl)
@@ -136,11 +192,12 @@ def openThread():
 		print("Sorry, thread is not up.")
 		print()
 
-
 def start():
 	importAPI()
 
 	extractJson()
+
+	extractQueue()
 
 	functionJson()
 
@@ -150,5 +207,5 @@ def start():
 
 	songTimeCurrent()
 
-if __name__ == "__main__":
+if __name__ == ("__main__"):
 	start()
