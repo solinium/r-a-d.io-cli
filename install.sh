602801c1c02f09b6d94470f77865a3498a151c63
Alexander <asg@airmail.cc>
2017-07-22 22:32:12 -0400

    add scripts

--- install.sh
+++ install.sh
@@ -5,7 +5,27 @@ git clone https://github.com/solinium/r-a-d.io-cli.git
 sudo mkdir -p /opt/r-a-d.io-cli/
 sudo cp $HOME/r-a-d.io-cli/cli.py /opt/r-a-d.io-cli/cli.py
 sudo cp $HOME/r-a-d.io-cli/audio.py /opt/r-a-d.io-cli/audio.py
-sudo cp $HOME/r-a-d.io-cli/start.sh /usr/bin/radiocli
+
+ret=`python -c 'import sys; print("%i" % (sys.hexversion<0x03000000))'`
+
+if [ $ret -eq 0 ]; then
+	{
+	sudo echo '#!/bin/sh'
+	sudo echo 'python /opt/r-a-d.io-cli/audio.py &'
+	sudo echo 'python /opt/r-a-d.io-cli/cli.py'                    
+	sudo echo 'clear'
+	} >/usr/bin/radiocli
+
+else 
+	{
+	sudo echo '#!/bin/sh'
+	sudo echo 'python3 /opt/r-a-d.io-cli/audio.py &'
+	sudo echo 'python3 /opt/r-a-d.io-cli/cli.py'                    
+	sudo echo 'clear'
+	} >/usr/bin/radiocli
+
+fi
+
 sudo chmod +x /usr/bin/radiocli
 rm -rf $HOME/r-a-d.io-cli/
 clear
