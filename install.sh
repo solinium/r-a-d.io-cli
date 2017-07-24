0dc0acbbd34a42a5762a78c9f04baa0009b2d095
Alexander <asg@airmail.cc>
2017-07-23 21:47:38 -0400

    fix install

--- install.sh
+++ install.sh
@@ -10,9 +10,9 @@ sudo cp $HOME/r-a-d.io-cli/audio.py /opt/r-a-d.io-cli/audio.py
 ret=`python -c 'import sys; print("%i" % (sys.hexversion<0x03000000))'`
 
 if [ $ret -eq 0 ]; then
-	sudo cp $HOME/r-a-d.io-cli/install/p2 /usr/bin/radiocli
+	sudo cp $HOME/r-a-d.io-cli/installfiles/p2 /usr/bin/radiocli
 else 
-	sudo cp $HOME/r-a-d.io-cli/install/p3 /usr/bin/radiocli
+	sudo cp $HOME/r-a-d.io-cli/installfiles/p3 /usr/bin/radiocli
 fi
 
 sudo chmod +x /usr/bin/radiocli
